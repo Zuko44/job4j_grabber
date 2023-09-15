02d6242440f3a9fe8c33addb61eaac5c6dc8140b
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-09-15 17:13:54 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.StringJoiner;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
@@ -28,7 +29,43 @@ public class HabrCareerParse {
                 Element published = row.select(".vacancy-card__date").get(0);
                 String publishedTwo = published.child(0).attr("datetime");
                 System.out.printf("Вакансия - %s, дата публикации - %s, %s%n", vacancyName, publishedTwo, link);
+                /**try {
+                 System.out.println(retrieveDescription(link));
+                 } catch (IOException e) {
+                 throw new RuntimeException(e);
+                 }*/
             });
         }
+    }
+
+    private String retrieveDescription(String link) throws IOException {
+        /**try {
+         URL url = new URL(link);
+         URLConnection urlConnection = url.openConnection();
+         HttpURLConnection connection = null;
+         if(urlConnection instanceof HttpURLConnection) {
+         connection = (HttpURLConnection) urlConnection;
+         }else {
+         //System.out.println("Пожалуйста, введите HTTP URL.");
+         return "Пожалуйста, введите HTTP URL.";
+         }
+         BufferedReader in = new BufferedReader(
+         new InputStreamReader(connection.getInputStream()));
+         String urlString = "";
+         String current;
+
+         while ((current = in.readLine()) != null) {
+         urlString += current;
+         }
+         return urlString;
+         } catch (IOException e) {
+         throw new RuntimeException(e);
+         }*/
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-description__text");
+        StringJoiner joiner = new StringJoiner("");
+        rows.forEach(row -> joiner.add(row.text()));
+        return joiner.toString();
     }
 }
